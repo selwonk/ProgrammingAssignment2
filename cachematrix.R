133f0d8e0a5bf8ceb3c559be4be888c79e8c30a8
Ryan Knowles <ryanbruceknowles@gmail.com>
2014-04-26 15:34:27 -0400

    Homework 2 - Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A module to invert square matricies while caching results.
+## The cache, set up in makeCacheMatrix, avoids repeating 
+## calculations by retrieving previously indexed results if available.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions that store the
+## initial matrix (in $set), allow retrieval of the matrix (in $get),
+## store the results of an inversion calculation (in $setInvert),
+## and allow retrieval of the results (in $getInvert).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    
+    ## store matrix
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    ## retrieve matrix
+    get <- function() x
+    
+    ## store result
+    setInvert <- function(invert) m <<- invert
+    
+    ## retrieve result
+    getInvert <- function() m
+    
+    ## create list
+    list(set = set, get = get,
+         setInvert = setInvert,
+         getInvert = getInvert)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first looks for a cached result using $getInvert()
+## from makeCacheMatrix. If it is not already calculated and
+## stored, it calculates the inversion using solve() and stores
+## the result using $setInvert().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## retrieve the result from x
+    m <- x$getInvert()
+    
+    ## if the result exists, return the result and exit
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    ## if the result does not exist, get the matrix from x
+    data <- x$get()
+    
+    ## calculate the inversion
+    m <- solve(data, ...)
+    
+    ## store the result
+    x$setInvert(m)
+    
+    ## return the result
+    m
 }
